d502f672c6071164341ce5d79812751a59d80b19
grisonrf <grisonrf@gmail.com>
2022-05-24 18:51:35 -0600

    Changes @ shoppingsite.py, 2nd & 3rd tasks

---
+++ customers.py
@@ -0,0 +1,12 @@
+"""Customers at Hackbright."""
+
+
+class Customer:
+    """Ubermelon customer."""
+
+    # TODO: need to implement this
+
+# checkout precisa ser implementado 
+# Add to cart precisa ser implementado
+# quando clica na melancia todas levam pra mesma pagina
+# cart com items que eu nao adicionei
